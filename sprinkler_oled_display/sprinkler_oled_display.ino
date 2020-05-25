3097ea69d5bbb78827d74d165f1e764708454031
bigendian <bigendian@gmail.com>
2020-05-25 10:39:23 -0700

    cleanups and add display to running modes

--- sprinkler_oled_display/sprinkler_oled_display.ino
+++ sprinkler_oled_display/sprinkler_oled_display.ino
@@ -21,14 +21,22 @@ Adafruit_SSD1306 display(OLED_RESET);
 int zone_state[10] = {
   ZONE_ON,
   ZONE_OFF,
+  ZONE_OFF,
+  ZONE_OFF,
+  ZONE_OFF,
+  ZONE_OFF,
+  ZONE_OFF,
+  ZONE_OFF,
   ZONE_ON,
   ZONE_OFF,
-  ZONE_ON,
-  ZONE_ON,
-  ZONE_OFF,
-  ZONE_OFF,
-  ZONE_OFF,
-  ZONE_OFF,
+};
+
+#define LINE_ONE 0
+#define LINE_TWO 1
+
+int display_offset[2] = {
+  0,
+  10,
 };
 
 #define MSG_RUNNING Running
@@ -49,15 +57,6 @@ void draw_running(int zone, int offset) {
     x = (zone - 5) * 13;
     y += 12;
   }
-//  Serial.print("zone ");
-//  Serial.print(zone);
-//  Serial.print("offset ");
-//  Serial.print(offset);
-//  Serial.print("  x: ");
-//  Serial.print(x);
-//  Serial.print("  y: ");
-//  Serial.println(y);
-  
   display.drawLine(x + offset,      y,               x + 10 - offset,  y + 10,      WHITE);
   display.drawLine(x,               y + 10 - offset, x + 10,           y + offset,  WHITE);
 }
@@ -68,41 +67,33 @@ void draw_running_orig(int x, int y, int offset) {
   display.drawLine(x,               y + 10 - offset, x + 10,           y + offset,  WHITE);
 }
 
-void print_to_oled(String str) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(str);
-
-  //  display.startscrollleft(0x00, 0x0F);
-
+void print_to_oled_at_y(String text, int yoffset) {
+  int x = 0;
+  display.setCursor(x, yoffset);
+  display.println(text);
 }
 
-
-// TODO: only turn on sprinklers running in zone_state
-
-
+void print_to_oled(String line_one, String line_two) {
+  display.fillRect(0, 0, display.width(), YLINE, BLACK);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  print_to_oled_at_y(line_one, display_offset[LINE_ONE]);
+  print_to_oled_at_y(line_two, display_offset[LINE_TWO]);
+}
 
 // animate running sprinklers
 void animate_sprinklers() {
-  while (1) {
-    // cycle through each animation step, skip 11 since this
-    // is the corner same as 0
-    for (int phase = 0; phase < 10; phase++) {
-      draw_display();
-      // cycle through all running sprinklers
-      for (int i = 0; i < sizeof(zone_state) / sizeof(zone_state[0]); i++) {
-        if (zone_state[i]) {
-          draw_running(i, phase);
-        }
+  // cycle through each animation step, skip 11 since this
+  // is the corner same as 0
+  for (int phase = 0; phase < 10; phase++) {
+    draw_display();
+    // cycle through all running sprinklers
+    for (int zone = 0; zone < sizeof(zone_state) / sizeof(zone_state[0]); zone++) {
+      if (zone_state[zone]) {
+        draw_running(zone, phase);
       }
-//      delay(10000);
-//            for (int x = 0; x + 11 < 65; x += 13) {
-//              draw_running_orig(x, YLINE + 2, phase);
-//              draw_running_orig(x, YLINE + 14, phase);
-//            }
-      display.display();
     }
+    display.display();
   }
 }
 
@@ -114,7 +105,6 @@ void draw_display() {
   }
 }
 
-
 void setup()   {
   Serial.begin(9600);
 
@@ -122,151 +112,15 @@ void setup()   {
   display.clearDisplay();
   delay(100);
 
-  print_to_oled("Running");
+  print_to_oled("Sprinkle", "Now!");
   display.display();
 
   draw_display();
   display.display();
   delay(2000);
-
-
-  // draw the first ~12 characters in the font
-  //  testdrawchar();
-  //  display.display();
-  //  delay(2000);
-  //  display.clearDisplay();
-
-  // draw scrolling text
-  //  testscrolltext();
-  //  delay(2000);
-  //  display.clearDisplay();
-
-  // text display tests
-  //  display.setTextSize(1);
-  //  display.setTextColor(WHITE);
-  //  display.setCursor(0, 0);
-  //  display.println("Hello, world!");
-  //  display.setTextColor(BLACK, WHITE); // 'inverted' text
-  //  display.println(3.141592);
-  //  display.setTextSize(2);
-  //  display.setTextColor(WHITE);
-  //  display.print("0x");
-  //  display.println(0xDEADBEEF, HEX);
-  //  display.display();
-  //  delay(2000);
-  //  display.clearDisplay();
-
 }
 
 void loop() {
+  print_to_oled("Running", "");
   animate_sprinklers();
 }
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-
-  for (uint8_t i = 0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }
-  display.display();
-  delay(1);
-}
-
-void testdrawcircle(void) {
-  for (int16_t i = 0; i < display.height(); i += 2) {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (int16_t i = 0; i < display.height() / 2; i += 3) {
-    // alternate colors
-    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, color % 2);
-    display.display();
-    delay(1);
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (int16_t i = 0; i < min(display.width(), display.height()) / 2; i += 5) {
-    display.drawTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = WHITE;
-  for (int16_t i = min(display.width(), display.height()) / 2; i > 0; i -= 5) {
-    display.fillTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawroundrect(void) {
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = WHITE;
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, color);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawrect(void) {
-  for (int16_t i = 0; i < display.height() / 2; i += 2) {
-    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-
-//void 2(void) {
-//  display.setTextSize(2);
-//  display.setTextColor(WHITE);
-//  display.setCursor(10, 0);
-//  //  display.clearDisplay();
-//  display.println("scroll");
-//  display.display();
-//  delay(1);
-//
-//  display.startscrollright(0x00, 0x0F);
-//  delay(2000);
-//  display.stopscroll();
-//  delay(1000);
-//  display.startscrollleft(0x00, 0x0F);
-//  delay(2000);
-//  display.stopscroll();
-//  delay(1000);
-//  display.startscrolldiagright(0x00, 0x07);
-//  delay(2000);
-//  display.startscrolldiagleft(0x00, 0x07);
-//  delay(2000);
-//  display.stopscroll();
-//}
